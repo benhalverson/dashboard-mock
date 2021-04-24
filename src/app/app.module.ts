4b6eabe2a5741bdc0e7758d0fa49ec7f65b30da8
Ben Halverson <benhalverson33@gmail.com>
2021-04-23 22:40:05 -0700

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MaterialModule } from './material-module';
+import { AccountSummaryComponent } from './components/account-summary/account-summary.component';
+import { AccountChartComponent } from './components/account-chart/account-chart.component';
+import { AccountTransactionsComponent } from './components/account-transactions/account-transactions.component';
+import { AccountPaymentsComponent } from './components/account-payments/account-payments.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AccountInfoComponent } from './components/account-info/account-info.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountSummaryComponent,
+    AccountChartComponent,
+    AccountTransactionsComponent,
+    AccountPaymentsComponent,
+    SidebarComponent,
+    AccountInfoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
